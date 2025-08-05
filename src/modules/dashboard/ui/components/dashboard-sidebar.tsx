1925df37cbf01c2bcba17255ac01836379cdc3a1
unknown <nafisahmed@iut-dhaka.edu>
2025-08-05 06:43:36 -0400

    dashboard sidebar created

---
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,137 @@
+"use client"
+
+import { Separator } from "@/components/ui/separator"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { cn } from "@/lib/utils"
+import { BotIcon, StarIcon, VideoIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { DashboardUserButton } from "./dashboard-user-button"
+// import { DashboardUserButton } from "./dashboard-user-button"
+// import { DashboardTrial } from "./dashboard-trial"
+
+
+const firstSection = [
+  {
+    icon: VideoIcon,
+    label: "Meetings",
+    href: "/meetings",
+  },
+  {
+    icon: BotIcon,
+    label: "Agents",
+    href: "/agents",
+  }
+]
+
+// const secondSection = [
+//   {
+//     icon: StarIcon,
+//     label: "Upgrade",
+//     href: "/upgrade",
+//   },
+// ]
+
+
+export const DashboardSidebar = () => {
+
+  const pathname = usePathname()
+  
+
+  return (
+    <Sidebar>
+      <SidebarHeader className="text-sidebar-accent-foreground">
+        <Link href="/" className="flex items-center gap-2 px-2 pt-2">
+          <Image 
+            src="logo.svg"
+            alt="logo"
+            height={42}
+            width={42}
+          />
+          <p className="text-xl font-semibold">
+            CoachPotato AI
+          </p>
+        </Link>
+      </SidebarHeader>
+
+      <div className="px-4 py-2">
+        <Separator className="opacity-10 text-[#6b5d5d]" />
+      </div>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {firstSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton 
+                    asChild
+                    className={cn(
+                      "h-10 border border-transparent hover:border-[#6b5d5d]/10 sidebarGradientButton transition-all duration-300",
+                      pathname === item.href && "border-l-4 border-l-orange-600  "
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5"/>
+                      <span className="text-sm font-medium tracking-tigh">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* <div className="px-4 py-2">
+          <Separator className="opacity-10 text-[#5D6B68]" />
+        </div> */}
+
+        {/* <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {secondSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    className={cn(
+                      "h-10 border border-transparent hover:border-[#5D6B68]/10 sidebarGradientButton transition-all duration-300",
+                      pathname === item.href && "border-l-4 border-l-orange-600  "
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tigh">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup> */}
+      </SidebarContent>
+
+      <SidebarFooter className="text-white">
+        {/* <DashboardTrial />   */}
+        <DashboardUserButton />
+      </SidebarFooter>
+
+    </Sidebar>
+  )
+}
---
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -0,0 +1,114 @@
+import { authClient } from "@/lib/auth-client"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon, Settings } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+
+
+
+export const DashboardUserButton = () => {
+
+  const router = useRouter();
+
+
+
+  const { data, isPending } = authClient.useSession();
+  console.log("data", data)
+  if(isPending || !data?.user){
+    return null
+  }
+
+  const onLogout = () => {
+    authClient.signOut({
+      fetchOptions:{
+        onSuccess: () => {
+          router.push("/sign-in")
+        }
+      }
+    })
+  }
+
+
+  // desktop -> dropdown
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger 
+        className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2 text-accent-foreground"
+      >
+        {data.user.image ? (
+          <Avatar className="size-9 mr-1">
+            <AvatarImage 
+              src={data.user.image}
+            />
+            <AvatarFallback>
+              <GeneratedAvatar
+                seed={data.user.name}
+                variant="initials"
+                className="size-full"
+              />
+            </AvatarFallback>
+          </Avatar>
+        ):(
+            <GeneratedAvatar 
+              seed={data.user.name}
+              variant="initials"
+              className="size-9 mr-3"
+            />
+        )}
+
+        <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+          <p className="text-sm truncate w-full">
+            {data.user.name}
+          </p>
+          <p className="text-xs truncate w-full">
+            {data.user.email}
+          </p>
+        </div>
+
+        <ChevronDownIcon className="size-4 shrink-0"/>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent 
+        align="end"
+        side="right"
+        className="w-72"
+      >
+        <DropdownMenuLabel>
+          <div className="flex flex-col gap-1">
+            <span className="font-medium truncate">{data.user.name}</span>
+            <span className="text-sm font-normal text-muted-foreground truncate">{data.user.email}</span>
+          </div>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem 
+        //   onClick={() => authClient.customer.portal()}
+          className="cursor-pointer flex items-center justify-between"
+        >
+          Settings
+          <Settings className="size-4"/>
+        </DropdownMenuItem>
+        <DropdownMenuItem 
+          onClick={onLogout}
+          className="cursor-pointer flex items-center justify-between"
+        >
+          Logout
+          <LogOutIcon className="size-4"/>
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+
+    </DropdownMenu>
+  )
+}
